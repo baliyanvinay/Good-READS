97b9f6af878e9f0884d20101072e75b5b12d682f
Vinay Baliyan <baliyanvinay17@outlook.com>
2020-10-19 17:32:10 +0530

    Redirected accounts/ to accounts/login using generic RedirectView

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+from django.views.generic import RedirectView
+# RedirectView will redirect url--> www.google.com/accounts/ to www.google.com/accounts/login/
+
 urlpatterns = [
+    path('', RedirectView.as_view(url='login/', permanent=True)),
     path('login/', views.login, name='login'),
     path('join/', views.join, name='join'),
 ]
