592de7283f5dd8d9e669071ac1e9b50a592ea57f
Vinay Baliyan <baliyanvinay17@outlook.com>
2020-10-22 10:37:01 +0530

    Return one line for all available genres for a book

--- books/models.py
+++ books/models.py
@@ -36,4 +36,4 @@ class Book(models.Model):
         In case there are more than one genre per book, all the available genres for a book are
         joined with comma and space. 
         '''
-        pass
+        return ', '.join(item for item in genre)
