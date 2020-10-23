4ce867d3906535ebb954fd8fc09c45c1412684e6
Vinay Baliyan <baliyanvinay17@outlook.com>
2020-10-23 22:24:14 +0530

    LoginView used for login

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from accounts.views import LoginView, JoinView, AuthorsView
+from accounts.views import JoinView, AuthorsView
+from django.contrib.auth.views import LoginView
 
 from django.views.generic import RedirectView
 # RedirectView will redirect url--> www.google.com/accounts/ to www.google.com/accounts/login/
@@ -7,7 +8,7 @@ from django.views.generic import RedirectView
 app_name = 'accounts'
 urlpatterns = [
     path('', RedirectView.as_view(url='login/', permanent=True)),
-    path('login/', LoginView.as_view(), name='login'),
+    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('join/', JoinView.as_view(), name='join'),
     path('authors/', AuthorsView.as_view(), name='authors'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -4,10 +4,6 @@ from django.views.generic import TemplateView, FormView
 from accounts.forms import JoinForm
 
 
-class LoginView(TemplateView):
-    template_name = 'accounts/login.html'
-
-
 class JoinView(FormView):
     template_name = 'accounts/signup.html'
     form_class = JoinForm
