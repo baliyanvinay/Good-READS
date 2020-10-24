3d5a2dd25326bc10607ae195dda05597c8a9902e
Vinay Baliyan <baliyanvinay17@outlook.com>
2020-10-24 09:52:52 +0530

    CustomUserAdmin to display extra fields

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from accounts.models import Account
 
-admin.site.register(Account, UserAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    model = Account
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('is_author', 'picture')}),)
+    list_display = ('first_name', 'last_name', 'username', 'is_author')
+
+
+admin.site.register(Account, CustomUserAdmin)
