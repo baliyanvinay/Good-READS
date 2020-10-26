b4bd11a5fc2020359f50bac3e0b38140abfa947c
Vinay Baliyan <baliyanvinay17@outlook.com>
2020-10-26 23:13:21 +0530

    password1 & password2 overridden to add placeholder in forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,6 +4,24 @@ from accounts.models import Account
 
 
 class JoinForm(UserCreationForm):
+    ''' password1 and password2 are created within UserCreationForm class, needed to override to 
+    add placeholder for display. 
+    Note that only password is saved in model and password1 and password2 are used for validation of
+    password only.
+    '''
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(
+            attrs={'autocomplete': 'new-password', 'placeholder': 'Enter Password'}),
+        strip=False,
+    )
+    password2 = forms.CharField(
+        label=("Password confirmation"),
+        widget=forms.PasswordInput(
+            attrs={'autocomplete': 'new-password', 'placeholder': 'Enter Password again'}),
+        strip=False,
+    )
+
     class Meta:
         model = Account
         fields = ['first_name', 'last_name',
@@ -13,5 +31,4 @@ class JoinForm(UserCreationForm):
             'first_name': forms.TextInput(attrs={'placeholder': 'Enter First Name', 'autofocus': 'autofocus'}),
             'last_name': forms.TextInput(attrs={'placeholder': 'Enter Last Name'}),
             'username': forms.TextInput(attrs={'placeholder': 'Enter Username'}),
-            'password1': forms.PasswordInput(attrs={'placeholder': 'Enter Username'}),
         }
