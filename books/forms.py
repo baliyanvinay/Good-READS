9bdeb4561a238b11656ad43f43adae8d8d7849c5
Vinay Baliyan <baliyanvinay17@outlook.com>
2020-10-28 23:18:31 +0530

    changed from authors to author in forms

--- books/forms.py
+++ books/forms.py
@@ -6,12 +6,12 @@ from django.forms import ModelForm
 
 class BookForm(ModelForm):
     # authors = forms.ModelMultipleChoiceField(queryset = Account.objects.filter(is_author=True))
-    authors = forms.ChoiceField(
+    author = forms.ChoiceField(
         choices=[(author.id, author.get_full_name) for author in Account.objects.filter(is_author=True)])
 
     class Meta:
         model = Book
-        fields = ('title', 'genre', 'authors', 'short_desc',
+        fields = ('title', 'genre', 'author', 'short_desc',
                   'description', 'ratings', 'cover_picture', 'copies', 'date_added')
         widgets = {
             'title': forms.TextInput(attrs={'placeholder': 'Enter Title', 'autofocus': 'autofocus'}),
