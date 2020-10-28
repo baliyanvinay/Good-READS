6ba0a896bb02d7736b45ce2ddb954eb37187c72c
Vinay Baliyan <baliyanvinay17@outlook.com>
2020-10-28 23:10:41 +0530

    Only authors to be displayed in add_book form

--- books/forms.py
+++ books/forms.py
@@ -1,12 +1,16 @@
 from django import forms
 from books.models import Book
+from accounts.models import Account
 from django.forms import ModelForm
 
 
 class BookForm(ModelForm):
+    authors = forms.ModelMultipleChoiceField(
+        queryset=Account.objects.filter(is_author=True))
+
     class Meta:
         model = Book
-        fields = ('title', 'genre', 'author', 'short_desc',
+        fields = ('title', 'genre', 'authors', 'short_desc',
                   'description', 'ratings', 'cover_picture', 'copies', 'date_added')
         widgets = {
             'title': forms.TextInput(attrs={'placeholder': 'Enter Title', 'autofocus': 'autofocus'}),
